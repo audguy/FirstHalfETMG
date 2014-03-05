cf964c378041fa37857357fda86ac176a81a4803
Darryl Malcomb <audguy@fullsail.edu>
2014-03-04 20:01:29 -0500

    Added Movement and walls
    Added Movement and walls, with scripts while testing for accelerometer
    input

---
+++ Week1and2/Assets/_Scripts/Follow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follow : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 Offset;
+	// Use this for initialization
+	void Start () {
+		Offset = transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = player.transform.position + Offset;
+	}
+}
---
+++ Week1and2/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float Scale=10;
+	void Update()
+	{/*
+		Vector3 acceleration = Vector3.zero;
+		int i = 0;
+		while (i < Input.accelerationEventCount) {
+			AccelerationEvent accEvent = Input.GetAccelerationEvent(i);
+			acceleration += accEvent.acceleration * accEvent.deltaTime;
+			++i;
+		}
+		//float moveH = Input.GetAxis ("Horizontal");
+		//float moveV = Input.GetAxis ("Vertical");
+		Vector3 scl = new Vector3 (Scale, Scale, Scale);
+
+		//acceleration.y = 0.0f;
+		//rigidbody.AddForce (acceleration *Scale,ForceMode.VelocityChange);
+		rigidbody.AddTorque (acceleration*Scale,ForceMode.VelocityChange);
+		*/
+		float mH = Input.GetAxis ("Horizontal");
+		float mV = Input.GetAxis ("Vertical");
+
+		Vector3 vec3 = new Vector3 (mH, 0.0f, mV);
+		rigidbody.AddForce(vec3 * Scale);
+	}
+}
