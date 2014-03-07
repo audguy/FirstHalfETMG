9a52489e61becc9a796a1135c983f67db84221f6
Darryl Malcomb <audguy@fullsail.edu>
2014-03-06 20:38:55 -0500

    Added GPS ramp

--- Week1and2/Assets/_Scripts/PlayerController.cs
+++ Week1and2/Assets/_Scripts/PlayerController.cs
@@ -4,7 +4,17 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	public float Scale=10;
-	void Update()
+	public GUIText Accel;
+	private bool isEnabled;
+	void Start()
+	{
+		isEnabled = Input.location.isEnabledByUser;
+		Input.location.Start ();
+
+		//LocationService ls = new LocationService ();
+		//ls.Start ();
+	}
+	void FixedUpdate()
 	{/*
 		Vector3 acceleration = Vector3.zero;
 		int i = 0;
@@ -21,10 +31,29 @@ public class PlayerController : MonoBehaviour {
 		//rigidbody.AddForce (acceleration *Scale,ForceMode.VelocityChange);
 		rigidbody.AddTorque (acceleration*Scale,ForceMode.VelocityChange);
 		*/
-		float mH = Input.GetAxis ("Horizontal");
-		float mV = Input.GetAxis ("Vertical");
+	
 
-		Vector3 vec3 = new Vector3 (mH, 0.0f, mV);
-		rigidbody.AddForce(vec3 * Scale);
+			Vector3 dir = Vector3.zero;
+			dir.z = Input.acceleration.y;
+			dir.x = Input.acceleration.x;
+		dir.y = 0;//Input.acceleration.z;
+			//if (dir.sqrMagnitude > 1)
+			//	dir.Normalize();
+
+		LocationInfo li = new LocationInfo ();
+		if (isEnabled && Input.location.status == LocationServiceStatus.Running)
+			Accel.text = dir.ToString () + "\nLat:" + Input.location.lastData.latitude + "\nLon:" + Input.location.lastData.longitude + "\nAlt:" + Input.location.lastData.altitude;
+				else if (Input.location.status == LocationServiceStatus.Initializing)
+						Accel.text = dir.ToString () + "\nLoc data Init";
+		else if (Input.location.status == LocationServiceStatus.Failed)
+			Accel.text = dir.ToString () + "\nLoc data Failed";
+		else if (Input.location.status == LocationServiceStatus.Stopped)
+			Accel.text = dir.ToString () + "\nLoc data Stopped";
+		else
+			Accel.text = dir.ToString () + "\nLoc data not enabled";
+		//dir *= Time.deltaTime;
+		rigidbody.AddForce (dir,ForceMode.VelocityChange);
+			//transform.Translate(dir * Scale);
+		}
 	}
-}
+	
