ef5d9a32777c52d44efef6933425ee884d72f1a0
Darryl Malcomb <audguy@fullsail.edu>
2014-03-09 13:55:25 -0400

    Comment clean-up
    Cleaned up the comments in the PlayerController

--- Week1and2/Assets/_Scripts/PlayerController.cs
+++ Week1and2/Assets/_Scripts/PlayerController.cs
@@ -15,23 +15,7 @@ public class PlayerController : MonoBehaviour {
 		//ls.Start ();
 	}
 	void FixedUpdate()
-	{/*
-		Vector3 acceleration = Vector3.zero;
-		int i = 0;
-		while (i < Input.accelerationEventCount) {
-			AccelerationEvent accEvent = Input.GetAccelerationEvent(i);
-			acceleration += accEvent.acceleration * accEvent.deltaTime;
-			++i;
-		}
-		//float moveH = Input.GetAxis ("Horizontal");
-		//float moveV = Input.GetAxis ("Vertical");
-		Vector3 scl = new Vector3 (Scale, Scale, Scale);
-
-		//acceleration.y = 0.0f;
-		//rigidbody.AddForce (acceleration *Scale,ForceMode.VelocityChange);
-		rigidbody.AddTorque (acceleration*Scale,ForceMode.VelocityChange);
-		*/
-	
+	{
 
 			Vector3 dir = Vector3.zero;
 			dir.z = Input.acceleration.y;
