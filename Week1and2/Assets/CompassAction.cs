03b0db1a1f3b8447c790542952efdfc6e5e622e8
Darryl Malcomb <audguy@fullsail.edu>
2014-03-13 17:56:18 -0400

    Skybox added
    Added a skybox, and other assets.

---
+++ Week1and2/Assets/CompassAction.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CompassAction : MonoBehaviour {
+	public GameObject camera;
+
+	private Vector3 Offset;
+	private float oldHdng;
+	// Use this for initialization
+	void Start () {
+		Offset = transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = camera.transform.position + Offset;
+		float newHeading = Input.compass.trueHeading;
+		if ((oldHdng - newHeading) > 3.0f || (oldHdng - newHeading) < -3.0f)
+			oldHdng = newHeading;
+		
+		transform.eulerAngles = new Vector3(0.0f, oldHdng, 0.0f);
+	
+
+	}
+}
--- Week1and2/Assets/_Scripts/Follow.cs
+++ Week1and2/Assets/_Scripts/Follow.cs
@@ -4,16 +4,20 @@ using System.Collections;
 public class Follow : MonoBehaviour {
 
 	public GameObject player;
+	public GUIText valu;
 	private Vector3 Offset;
+	private float oldHdng;
 	// Use this for initialization
 	void Start () {
 		Offset = transform.position;
 		Input.compass.enabled = true;
+		oldHdng = 0.0f;
 	}
 	
 	// Update is called once per frame
 	void LateUpdate () {
+
 		transform.position = player.transform.position + Offset;
-		transform.localRotation.Set (0.0f, Input.compass.trueHeading/360.0f, 0.0f, 0.0f);
+
 	}
 }
