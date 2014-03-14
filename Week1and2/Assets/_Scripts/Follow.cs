d8a58ff1133c81c8e5cff5d39ea9f61b9e6bf802
audguy <audguy@audguy.com>
2014-03-14 14:22:38 -0400

    transfer
    backup

--- Week1and2/Assets/_Scripts/Follow.cs
+++ Week1and2/Assets/_Scripts/Follow.cs
@@ -6,18 +6,32 @@ public class Follow : MonoBehaviour {
 	public GameObject player;
 	public GUIText valu;
 	private Vector3 Offset;
-	private float oldHdng;
+	private float chngHdng;
+
 	// Use this for initialization
 	void Start () {
 		Offset = transform.position;
-		Input.compass.enabled = true;
-		oldHdng = 0.0f;
+	
 	}
 	
 	// Update is called once per frame
-	void LateUpdate () {
+	void Update () {
 
-		transform.position = player.transform.position + Offset;
+		//
+		transform.position = player.transform.position;
+
+		transform.rotation.Set (transform.rotation.x, 0.0f, transform.rotation.z, 0.0f);
+		//transform.RotateAround(player.rigidbody.position, Vector3.up, chngHdng);
+		transform.position.Set(player.transform.position.x + Offset.x,player.transform.position.y + Offset.y,player.transform.position.z + Offset.z);
+		valu.text = player.transform.position.ToString();
+
+		chngHdng = 0.0f;
+	}
+
+	void OnGUI()
+	{
+		if (GUI.RepeatButton (new Rect (5, 5, (Screen.width / 10) + 5, Screen.height - 10), "<") == true)
+						chngHdng = -1.0f;
 
 	}
 }
